a493022f59c4a727dfef80e390d8dc1213340f2b
li_camile <2645093633@qq.com>
2023-12-25 21:29:41 +0800

    向app注入store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import store from "./store";
+import { Provider } from "react-redux";
 
-import App from './App'
+import App from "./App";
 
-
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById("root"));
 root.render(
-  <App />
-)
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
