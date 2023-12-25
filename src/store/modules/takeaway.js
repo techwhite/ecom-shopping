42fd63f97287893dc8be1ee5947660f018f3f000
li_camile <2645093633@qq.com>
2023-12-25 21:17:01 +0800

    modify takeaway.js
    add foods redux and fetchFoodList function

---
+++ src/store/modules/takeaway.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const foodStore = createSlice({
+  name: "foods",
+  initialState: {
+    foodList: [],
+  },
+  reducers: {
+    setFoodList(state, action) {
+      state.foodList = action.payload;
+    },
+  },
+});
+
+const { setFoodList } = foodStore.actions;
+
+const fetchFoodList = () => {
+  return async (dispatch) => {
+    const res = await axios.get(" http://localhost:3004/takeaway");
+    setFoodList(res.data);
+  };
+};
+
+export { fetchFoodList };
+export default foodStore;
